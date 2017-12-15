34dc08e13c71227d8c8ab77880a5a0625366c130
AlianWen <443129953@qq.com>
2017-12-15 09:15:13 +0800

    Create useralive.sql

---
+++ CRtable/Procedure/userDeal/useralive.sql
@@ -0,0 +1,11 @@
+BEGIN
+	#Routine body goes here...
+	CALL userpackagelog();
+	CALL useraliveday();
+	CALL userdaynew();
+	CALL useralivecount();
+	CALL useralivehuorly();
+	CALL userpackagecount();
+	
+
+END
