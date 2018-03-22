c30baee98142784a35cb4c5bd521def4a24159d1
AlianWen <443129953@qq.com>
2018-03-22 10:14:26 +0800

    Update product_stw_kpicount.sql

--- XHProductbase/BI_stw/product_stw_kpicount.sql
+++ XHProductbase/BI_stw/product_stw_kpicount.sql
@@ -1,44 +1,47 @@
-INSERT INTO product_stw_kpicount 
-SELECT a3.teacherid,a3.teachername,a3.classid,a3.classname,a3.schoolid,a3.studentnum,NVL(stab1.subjectid,0),
-NVL(stab1.zilnum,0),NVL(stab1.counthw,0),NVL(stab1.countgame,0),NVL(stab1.finrate,0),NVL(stab1.taskrate,0),NVL(stab1.datetime,0),
-from_unixtime(unix_timestamp()-60*60*24*0,'yyyyMMdd') AS uptime
-FROM teacher_info_stunum a3
-LEFT JOIN
-(SELECT sta1.teacherid,sta1.subjectid,sta3.zilnum,sta1.counthw,sta1.countgame,
-sta2.allfin AS taskrate,sta2.finsum AS finrate,sta1.datetime FROM
-(SELECT a1.teacherid,a3.subjectid,a1.datetime,
-count(a1.homeworkid) AS counthw,SUM(a1.gamecount) AS countgame
-FROM(SELECT DISTINCT teacherid,bookid,homeworkid,gamecount,
-FROM_UNIXTIME(int(createtime/1000),'yyyyMMdd') AS datetime
-FROM product_stw_kpibase ) a1
-LEFT JOIN product_stw_subject a3 ON a1.bookid=a3.bookid
-GROUP BY a1.teacherid,a3.subjectid,a1.datetime)sta1
-LEFT JOIN
-(SELECT a2.teacherid,a3.subjectid,
-FROM_UNIXTIME(int(a2.createtime/1000),'yyyyMMdd') AS datetime,
-SUM(IF(a2.isfinish=1,1,0))AS finsum,COUNT(a2.isfinish) AS allfin,
-SUM(a2.finishcount)AS gamefin, SUM(a2.gamecount)AS gamesum
-FROM product_stw_kpibase a2 
-LEFT JOIN product_stw_subject a3 ON a2.bookid=a3.bookid
-GROUP BY FROM_UNIXTIME(int(a2.createtime/1000),'yyyyMMdd'),
-a2.teacherid,a3.subjectid)sta2 
-ON sta1.teacherid=sta2.teacherid AND sta1.subjectid=sta2.subjectid
-AND sta1.datetime=sta2.datetime 
-LEFT JOIN(
-SELECT a3.teacherid,sta4.subjectid,sum(sta4.zlnum)AS zilnum,sta4.datetime
-FROM teacher_student_info a3
-LEFT JOIN
-(SELECT a1.userid,a2.subjectid,count(DISTINCT a1.id)AS zlnum,
-from_unixtime(a1.createtime,'yyyyMMdd') AS datetime
-FROM product_stw_base a1
-LEFT JOIN product_stw_subject a2
-ON a1.bookid=a2.bookid
-WHERE a1.ishomework='1' AND
-a1.udatetime>=from_unixtime(unix_timestamp()-60*60*24*0,'yyyyMMdd')
-GROUP BY a1.userid,a2.subjectid,from_unixtime(a1.createtime,'yyyyMMdd'))sta4
-ON sta4.userid=a3.userid WHERE sta4.subjectid IS NOT NULL 
-AND a3.datetime>=from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
-GROUP BY a3.teacherid,sta4.subjectid,sta4.datetime) sta3 
-ON sta1.teacherid=sta3.teacherid AND sta1.subjectid=sta3.subjectid AND sta1.datetime=sta3.datetime
-)stab1 ON stab1.teacherid=a3.teacherid
-WHERE a3.datetime>=from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+INSERT INTO product_stw_kpicount
+SELECT tab1.teacherid,tab1.teachername,tab1.classid,tab1.classname,tab3.studentnum,tab1.schoolid,
+tab1.schoolname,tab1.subjectid,SUM(tab1.ziliancishu)AS zilcishu,SUM(tab1.buzhicishu)AS buzcishu,
+SUM(tab1.buzhifenshu)AS buzfenshu,SUM(tab1.shangjiaofenshu)AS shangjfenshu,tab1.datetime,
+FROM_UNIXTIME(UNIX_TIMESTAMP()-24*60*60*0,'yyyyMMdd') AS uptime
+FROM
+(SELECT sta2.teacherid,sta3.teachername,sta2.classid,sta3.classname,
+sta3.schoolid,sta3.schoolname,sta2.subjectid,0 AS ziliancishu,
+sta2.buzhicishu,sta2.shangjiaofenshu,sta2.buzhifenshu,sta2.datetime
+FROM 
+(SELECT st1.teacherid,st1.classid,st1.subjectid,
+FROM_UNIXTIME(int(st1.createtime/1000),'yyyyMMdd') AS datetime,
+COUNT(DISTINCT st1.homeworkid) AS buzhicishu,
+SUM(st1.isfinish)AS shangjiaofenshu, COUNT(st1.isfinish)AS buzhifenshu
+FROM(SELECT a1.userid,a1.createtime,a1.homeworkid,a1.teacherid,
+a1.isfinish,a1.bookid,a2.classid,a3.subjectid
+FROM product_stw_kpibase a1 LEFT JOIN teacher_student_info a2 
+ON a2.userid=a1.userid AND a1.teacherid=a2.teacherid
+LEFT JOIN product_stw_subject a3 ON a3.bookid=a1.bookid)AS st1
+WHERE st1.classid IS NOT NULL
+GROUP BY st1.teacherid,st1.classid,st1.subjectid,
+FROM_UNIXTIME(int(st1.createtime/1000),'yyyyMMdd'))sta2
+LEFT JOIN teacher_info_stunum sta3 
+ON sta3.teacherid=sta2.teacherid AND sta3.classid=sta2.classid
+WHERE sta3.datetime>=from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+UNION ALL
+SELECT sta3.teacherid,sta3.teachername,sta4.classid,sta3.classname,
+sta3.schoolid,sta3.schoolname,sta4.subjectid,sta4.ziliancishu,
+0 AS buzhicishu,0 AS shangjiaofenshu,0 AS buzhifenshu,sta4.datetime
+FROM 
+(SELECT sta1.classid,sta1.subjectid,
+FROM_UNIXTIME(sta1.createtime,'yyyyMMdd') AS datetime,
+COUNT(DISTINCT sta1.id) AS ziliancishu
+FROM(SELECT a4.userid,a4.createtime,a4.id,
+a4.bookid,a2.classid,a3.subjectid
+FROM product_stw_base a4 LEFT JOIN teacher_student_info a2 
+ON a2.userid=a4.userid LEFT JOIN product_stw_subject a3 
+ON a3.bookid=a4.bookid WHERE a4.ishomework=1 )AS sta1
+WHERE sta1.classid IS NOT NULL
+GROUP BY sta1.classid,sta1.subjectid,
+FROM_UNIXTIME(sta1.createtime,'yyyyMMdd'))sta4
+LEFT JOIN teacher_info_stunum sta3 
+ON sta3.classid=sta4.classid
+WHERE sta3.datetime>=from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd'))tab1
+LEFT JOIN teacher_info_stunum tab3 ON tab1.classid=tab3.classid AND tab1.teacherid=tab3.teacherid
+GROUP BY tab1.teacherid,tab1.teachername,tab1.classid,tab1.classname,tab1.schoolid,
+tab1.schoolname,tab1.subjectid, tab1.datetime,tab3.studentnum
