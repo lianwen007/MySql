ea923ad46f4ba32d4fcca16604f0963e618f753b
AlianWen <443129953@qq.com>
2018-04-03 15:59:21 +0800

    Create product_stw_subcount.sql

---
+++ XHProductbase/BI_stw/product_stw_subcount.sql
@@ -0,0 +1,14 @@
+BEGIN
+INSERT INTO  product_stw_subcount(
+SELECT sta1.userid,sta1.username,sta1.subjectid,
+sta1.schoolid,sta1.classid,sta1.classname,
+sum(sta1.topicnum)AS alltopicnum,sum(countright)AS numright,sta1.datetime FROM(
+SELECT a1.userid,a1.username,a2.subjectid,a1.schoolid,a1.classid,
+a1.classname,a1.topicnum,a1.countright,a1.datetime
+FROM product_stw_daycount a1 
+LEFT JOIN product_stw_subject a2
+ON a1.bookid=a2.bookid)AS sta1
+GROUP BY sta1.userid,sta1.subjectid,sta1.datetime,sta1.username,
+sta1.schoolid,sta1.classid,sta1.classname
+);
+END
