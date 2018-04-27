1e3956b521af12dd2f4285cac64233625c697411
AlianWen <443129953@qq.com>
2018-04-27 09:34:29 +0800

    Update ctl.sql

--- XHProductbase/BI_stw/ctl.sql
+++ XHProductbase/BI_stw/ctl.sql
@@ -52,7 +52,7 @@ CASE WHEN a3.subtype = 2 THEN COUNT(a3.ishomework)*10
 WHEN a3.subtype = 1 THEN (COUNT(a3.ishomework)*10 - SUM(IF(a3.judgecount>0,a3.judgecount,0))) 
 END AS numtopic,
 SUM(a3.rightnum) AS sumright,
-AVG(a3.judgecount)AS engrightrate,
+SUM(a3.judgecount)AS engrightrate,
 SUM(a3.timecost) AS sumtime,
 a3.subtype
 FROM(SELECT a33.*,a44.subtype FROM
@@ -88,6 +88,7 @@ WHERE udatetime >= unix_timestamp(from_unixtime(unix_timestamp()-60*60*24*0,'yyy
 on a6.userid=a7.userid and a6.bookid=a7.bookid and a6.updatetime=a7.maxtime
 WHERE a6.udatetime >= unix_timestamp(from_unixtime(unix_timestamp()-60*60*24*0,'yyyy-MM-dd'),'yyyy-MM-dd')
 )sta6 ON sta1.userid = sta6.userid AND sta1.bookid=sta6.bookid;
+
 TRUNCATE TABLE product_stw_kpicount;
 INSERT INTO product_stw_kpicount
 SELECT tab1.teacherid,tab1.teachername,tab1.classid,tab1.classname,tab3.studentnum,tab1.schoolid,
@@ -138,87 +139,73 @@ GROUP BY tab1.teacherid,tab1.teachername,tab1.classid,tab1.classname,tab1.school
 tab1.schoolname,tab1.subjectid, tab1.datetime,tab3.studentnum;
 
 TRUNCATE TABLE product_stw_encount;
-SELECT s1.userid,s2.username,s2.schoolid,s2.classid,s2.classname,
-SUM(s1.gamecounts)AS gamecount,
-SUM(s1.finishcounts)AS fincount,
-SUM(s1.homefullcounts)AS homefull,
-SUM(s1.selfcounts)AS selfcount,
-SUM(s1.selffullcounts)AS selffull,
-SUM(NVL(s1.listenfull,0))AS listenfull,
-SUM(NVL(s1.readfull,0))AS readfull,
-SUM(NVL(s1.wanxinfull,0))AS blankfull,
-AVG(NVL(s1.listenrate,0))AS ratelisten,
-AVG(NVL(s1.readrate,0))AS rateread,
-AVG(NVL(s1.wanxinrate,0))AS rateblank,
-SUM(NVL(s1.listennum,0))AS numlisten,
-SUM(NVL(s1.readnum,0))AS numread,
-SUM(NVL(s1.wanxinnum,0))AS numblank,
-s1.datetime
+INSERT INTO product_stw_encount
+SELECT stab1.userid,s2.username,s2.schoolid,s2.classid,s2.classname,
+NVL(stab2.pubcount,0)AS gamecount,
+NVL(stab2.fincount,0)AS fincount,
+SUM(NVL(stab1.homewkfull,0))AS homefull,
+SUM(NVL(stab1.selfnum,0))AS selfcount,
+SUM(NVL(stab1.fullself,0))AS selffull,
+SUM(NVL(stab1.listenfull,0))AS listenfull,
+SUM(NVL(stab1.readfull,0))AS readfull,
+SUM(NVL(stab1.blankfull,0))AS blankfull,
+NVL(AVG(stab1.listenrate),0)AS ratelisten,
+NVL(AVG(stab1.readrate),0)AS rateread,
+NVL(AVG(stab1.blankrate),0)AS rateblank,
+SUM(NVL(stab1.listennum,0))AS numlisten,
+SUM(NVL(stab1.readnum,0))AS numread,
+SUM(NVL(stab1.blanknum,0))AS numblank,
+SUM(NVL(stab1.avgrate,0))AS rateavg,
+stab1.datetime
 FROM(
-SELECT stabl1.userid,stabl1.bookid,stabl1.typeen,stabl1.datetime,
-SUM(stabl1.gamect)AS gamecounts,
-SUM(stabl1.finishct)AS finishcounts, 
-SUM(NVL(stabl1.homefullct,0))AS homefullcounts,
-SUM(stabl1.selfct)AS selfcounts,
-SUM(NVL(stabl1.selffullct,0))AS selffullcounts,
-CASE WHEN stabl1.typeen = '9' THEN
-(SUM(NVL(stabl1.homefullct,0))+SUM(NVL(stabl1.selffullct,0))) END AS listenfull,
-CASE WHEN stabl1.typeen = '7' THEN
-(SUM(NVL(stabl1.homefullct,0))+SUM(NVL(stabl1.selffullct,0))) END AS readfull,
-CASE WHEN stabl1.typeen = '14' THEN
-(SUM(NVL(stabl1.homefullct,0))+SUM(NVL(stabl1.selffullct,0))) END AS wanxinfull,
-CASE WHEN stabl1.typeen = '9' THEN
-AVG(NVL(stabl1.avgallrate,0)) END AS listenrate,
-CASE WHEN stabl1.typeen = '7' THEN
-AVG(NVL(stabl1.avgallrate,0)) END AS readrate,
-CASE WHEN stabl1.typeen = '14' THEN
-AVG(NVL(stabl1.avgallrate,0)) END AS wanxinrate,
-CASE WHEN stabl1.typeen = '9' THEN
-SUM(NVL(stabl1.numall,0)) END AS listennum,
-CASE WHEN stabl1.typeen = '7' THEN
-SUM(NVL(stabl1.numall,0)) END AS readnum,
-CASE WHEN stabl1.typeen = '14' THEN
-SUM(NVL(stabl1.numall,0)) END AS wanxinnum
-FROM(
-SELECT stab1.userid,stab1.bookid,stab1.typeen,stab1.ishomewk,stab1.datetime,
-SUM(stab1.gamecount)AS gamect,
-SUM(stab1.finishcount)AS finishct,
-CASE WHEN stab1.ishomewk = '0' THEN 
-SUM(IF(stab1.judgecount='100',1,0)) END AS homefullct,
-SUM(IF(stab1.ishomewk=1,1,0))AS selfct,
-CASE WHEN stab1.ishomewk = '1' THEN 
-SUM(IF(stab1.judgecount='100',1,0)) END AS selffullct,
-AVG(stab1.judgecount) avgallrate,
-COUNT(stab1.ishomewk) numall
-FROM
-(SELECT sta0.id,sta0.datetime,sta0.userid,sta0.bookid,sta0.judgecount,
-NVL(sta2.gamecount,0)AS gamecount,
-NVL(sta2.finishcount,0)AS finishcount,
-NVL(sta1.ishomework,sta2.ishomework) AS ishomewk,
-NVL(sta1.entype,sta2.entype)AS typeen
-FROM
-(SELECT a0.id,a0.userid,a0.bookid,
-FROM_UNIXTIME(a0.createtime,'yyyy-MM-dd') AS datetime,a0.judgecount
-FROM product_stw_base a0 WHERE
-a0.udatetime>=unix_timestamp(from_unixtime(unix_timestamp(),'yyyy-MM-dd'),'yyyy-MM-dd')
-) sta0 LEFT JOIN
-(SELECT a1.id,a1.entype,1 AS ishomework
-FROM product_stw_entype a1
-WHERE a1.homeworkid='0' AND a1.entype!=0
-) sta1 ON sta0.id=sta1.id
+SELECT sta7.userid,sta7.datetime,sta7.entype,sta7.ishomework,
+SUM(sta7.judgecount)AS avgrate,
+CASE WHEN sta7.ishomework = '1' THEN 
+COUNT(sta7.ishomework) END AS selfnum,
+CASE WHEN sta7.ishomework = '0' THEN 
+SUM(IF(sta7.judgecount = '100',1,0)) END AS homewkfull,
+CASE WHEN sta7.ishomework = '1' THEN 
+SUM(IF(sta7.judgecount = '100',1,0)) END AS fullself,
+CASE WHEN sta7.entype = '9' THEN
+AVG(sta7.judgecount) END AS listenrate,
+CASE WHEN sta7.entype = '7' THEN
+AVG(sta7.judgecount) END AS readrate,
+CASE WHEN sta7.entype = '14' THEN
+AVG(sta7.judgecount) END AS blankrate,
+CASE WHEN sta7.entype = '9' THEN
+SUM(IF(sta7.judgecount='100',1,0)) END AS listenfull,
+CASE WHEN sta7.entype = '7' THEN
+SUM(IF(sta7.judgecount='100',1,0)) END AS readfull,
+CASE WHEN sta7.entype = '14' THEN
+SUM(IF(sta7.judgecount='100',1,0)) END AS blankfull,
+CASE WHEN sta7.entype = '9' THEN
+COUNT(sta7.ishomework) END AS listennum,
+CASE WHEN sta7.entype = '7' THENkok
+COUNT(sta7.ishomework) END AS readnum,
+CASE WHEN sta7.entype = '14' THEN
+COUNT(sta7.ishomework) END AS blanknum
+FROM(SELECT a7.userid,a7.judgecount,a7.ishomework,a8.entype,a9.subjectid,
+FROM_UNIXTIME(a7.createtime,'yyyy-MM-dd') AS datetime
+FROM product_stw_base a7 
+LEFT JOIN product_stw_entype a8
+ON a7.id = a8.id 
+JOIN product_stw_subject a9
+ON a7.bookid = a9.bookid
+AND a7.udatetime>unix_timestamp(from_unixtime(unix_timestamp(),'yyyy-MM-dd'),'yyyy-MM-dd')
+AND a9.subjectid = '3') sta7
+GROUP BY sta7.userid,sta7.datetime,sta7.entype,sta7.ishomework
+) stab1 LEFT JOIN
+(SELECT a4.userid,sum(a4.gamecount)AS pubcount,
+sum(a4.finishcount)AS fincount,
+FROM_UNIXTIME(int(a4.createtime/1000),'yyyy-MM-dd') AS datetime
+FROM product_stw_kpibase a4 JOIN product_stw_subject a3
+ON a3.bookid = a4.bookid and a3.subjectid = '3'
+GROUP BY a4.userid,a4.bookid,FROM_UNIXTIME(int(a4.createtime/1000),'yyyy-MM-dd')
+) stab2 ON stab1.userid = stab2.userid and stab1.datetime = stab2.datetime
 LEFT JOIN
-(SELECT a3.id,a4.gamecount,a4.finishcount,a3.entype,0 AS ishomework
-FROM product_stw_entype a3 JOIN product_stw_kpibase a4
-ON a3.homeworkid=a4.id
-WHERE a3.homeworkid!='0' AND a3.entype!=0
-)sta2 ON sta0.id = sta2.id
-WHERE sta1.entype IS NOT NULL OR sta2.entype IS NOT NULL
-)stab1
-GROUP BY stab1.userid,stab1.bookid,stab1.typeen,stab1.ishomewk,stab1.datetime
-) stabl1 
-GROUP BY stabl1.userid,stabl1.bookid,stabl1.typeen,stabl1.datetime
-) s1 LEFT JOIN 
 (SELECT DISTINCT sa2.userid,sa2.username,sa2.schoolid,sa2.classid,sa2.classname
 FROM teacher_student_info sa2 
-)s2 ON s1.userid=s2.userid
-GROUP BY s1.userid,s2.username,s2.schoolid,s2.classid,s2.classname,s1.datetime;
+)s2 ON stab1.userid=s2.userid
+GROUP BY stab1.userid,s2.username,NVL(stab2.fincount,0),
+s2.schoolid,s2.classid,NVL(stab2.pubcount,0),
+s2.classname,stab1.datetime;
