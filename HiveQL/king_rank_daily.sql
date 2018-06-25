a2afc7f3379e1c07c2a0f0100b292b545f57bca8
AlianWen <443129953@qq.com>
2018-06-25 14:23:56 +0800

    Update king_rank_daily.sql

--- HiveQL/king_rank_daily.sql
+++ HiveQL/king_rank_daily.sql
@@ -1,117 +1,167 @@
+-- Hive Create Table
 CREATE TABLE rptdata.fact_king_rank_daily(
-  id string,
-  school_id string,
+  id bigint,
+  school_id bigint,
   book_id string,
-  subject_id string,
-  class_id string,
+  subject_id bigint,
+  class_id bigint,
   class_name string,
-  student_id string,
+  student_id bigint,
   student_name string,
-  intgral string,
-  qst_count string,
-  qst_right string,
-  qst_right_tate string,
-  class_rank string,
-  class_compare string,
-  school_rank string,
-  school_compare string,
+  intgral int,
+  qst_count int,
+  qst_right int,
+  qst_right_rate string,
   is_weekly string
   )  
 PARTITIONED BY (date_time string)  
 STORED AS PARQUET;
-  
-  
+
+-- MYSQL Create Table
+CREATE TABLE `king_rank_daily` (
+  `id` bigint(20) NOT NULL COMMENT 'ID',
+  `school_id` bigint(20) DEFAULT NULL COMMENT '学校标识',
+  `book_id` varchar(30) DEFAULT NULL COMMENT '书本标识',
+  `subject_id` bigint(20) DEFAULT NULL COMMENT '科目标识',
+  `class_id` bigint(20) DEFAULT NULL COMMENT '班级标识',
+  `class_name` varchar(100) DEFAULT NULL COMMENT '班级名称',
+  `student_id` bigint(20) DEFAULT NULL COMMENT '学生标识',
+  `student_name` varchar(100) DEFAULT NULL COMMENT '学生名称',
+  `intgral` int(10) DEFAULT NULL COMMENT '积分',
+  `qst_count` int(10) DEFAULT NULL COMMENT '做题量',
+  `qst_right` int(10) DEFAULT NULL COMMENT '正确题量',
+  `qst_right_rate` varchar(10) DEFAULT NULL COMMENT '正确率',
+  `is_weekly` varchar(1) NOT NULL COMMENT '周标识(1:历史所有,2:上周,3:本周,4:上上周)',
+  `date_time` varchar(8) NOT NULL COMMENT '统计日期',
+  KEY `index_school_id_only` (`school_id`),
+  KEY `index_book_id_only` (`book_id`),  
+  KEY `index_class_id_only` (`class_id`),
+  KEY `index_student_id_only` (`student_id`),
+  KEY `index_is_weekly_only` (`is_weekly`),
+  KEY `index_date_time_only` (`date_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+   
 SET hive.exec.dynamic.partition.mode=nonstrict; 
 insert overwrite table rptdata.fact_king_rank_daily partition(date_time)
-SELECT row_number() over() as row_id,st0.*
-FROM(
--- onweek records
-SELECT -- row_number() over() as row_id,
-stab1.school_id,stab1.book_id,stab1.subject_id,stab1.class_id,stab1.class_name,
-stab1.student_id,stab1.student_name,stab1.integral,stab1.qst_num,0 qst_right,0 qst_right_rate,
-stab1.class_rank,int(stab1.class_rank - NVL(stab2.class_rank,stab1.class_rank)) class_compare,
-stab1.school_rank,int(stab1.school_rank - NVL(stab2.school_rank,stab1.school_rank)) school_compare,
-1 is_weekly,stab1.date_time
-FROM 
-(SELECT 
-sta1.school_id,sta1.book_id,sta1.subject_id,sta1.class_id,sta1.class_name,
-sta1.student_id,sta1.student_name,sta1.integral,sta1.qst_num,0 qst_right,0 qst_right_rate,
-row_number() over(partition by sta1.class_id,sta1.book_id 
-order by sta1.integral desc) class_rank,
-row_number() over(partition by sta1.school_id,sta1.book_id 
-order by sta1.integral desc) school_rank,
+SELECT row_number() over() as row_id,
+st3.school_id,st0.book_id,st0.subject_id,st0.class_id,st3.class_name,
+st0.student_id,st3.student_name,st0.integral,st0.qst_num,st0.qst_right,
+st0.qst_right_rate,st0.is_weekly,
 from_unixtime(unix_timestamp(),'yyyyMMdd') date_time
 FROM(
-SELECT a1.student_id,a3.student_name,a1.class_id,a3.class_name,a3.school_id,
+-- onweek records
+SELECT
+sta1.book_id,sta1.subject_id,sta1.class_id,
+sta1.student_id,sta1.integral,sta1.qst_num,INT(sta1.qst_right/100) AS qst_right,
+INT(sta1.qst_right*10/sta1.qst_num)/10 AS qst_right_rate, 3 AS is_weekly
+FROM(
+SELECT a1.student_id,a1.class_id,
 a1.book_id,a2.subject_id,a2.book_type,
   SUM(a1.integral) integral,   
 CASE WHEN a2.book_type IN (1,3) THEN COUNT(a1.game_id)*10
     WHEN a2.book_type IN (2,5) THEN COUNT(a1.game_id)
-    END AS qst_num
+    END AS qst_num,
+CASE WHEN a2.book_type IN (1,3) THEN SUM(question_right)*100
+    WHEN a2.book_type IN (2,5) THEN SUM(accuracy)
+    ELSE 0 
+    END AS qst_right
 FROM rptdata.fact_king_game_detail a1 
 JOIN rptdata.fact_king_book_library_detail a2 ON a1.book_id = a2.book_id
-LEFT JOIN (
-SELECT DISTINCT ta3.student_id,ta3.student_name,
-ta3.class_name,ta3.school_id
-FROM rptdata.fact_king_student_detail ta3
-) a3 
-ON a1.student_id = a3.student_id
 WHERE a1.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
 AND a2.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
 AND a1.create_time>=(unix_timestamp(substr(from_unixtime(unix_timestamp()),1,4),'yyyy')+
           ((weekofyear(from_unixtime(unix_timestamp(),'yyyy-MM-dd'))-1)*86400*7))*1000
 AND a1.create_time<(unix_timestamp(substr(from_unixtime(unix_timestamp()),1,4),'yyyy')+
           ((weekofyear(from_unixtime(unix_timestamp(),'yyyy-MM-dd'))-0)*86400*7))*1000
-GROUP BY a1.student_id,a3.student_name,a1.class_id,a3.class_name,a3.school_id,
-a1.book_id,a2.subject_id,a2.book_type
-  ) sta1
-)stab1
-LEFT JOIN 
-(SELECT 
-a5.student_id,a5.book_id,a5.class_rank,a5.school_rank
-FROM
-  rptdata.fact_king_rank_last_week a5
-WHERE a5.date_time = FROM_UNIXTIME((unix_timestamp(substr(from_unixtime(unix_timestamp()),1,4),'yyyy')+
-          ((weekofyear(from_unixtime(unix_timestamp(),'yyyy-MM-dd'))-1)*86400*7)),'yyyyMMdd')
-)stab2
-ON stab1.student_id = stab2.student_id AND stab1.book_id = stab2.book_id
+GROUP BY a1.student_id,a1.class_id,a1.book_id,a2.subject_id,a2.book_type
+) sta1
 
-UNION ALL 
--- historic records
-SELECT -- row_number() over() as row_id,
-stab1.school_id,stab1.book_id,stab1.subject_id,stab1.class_id,stab1.class_name,
-stab1.student_id,stab1.student_name,stab1.integral,stab1.qst_num,0 qst_right,0 qst_right_rate,
-stab1.class_rank,0 class_compare,
-stab1.school_rank,0 school_compare,
-0 is_weekly,stab1.date_time
-FROM 
-(SELECT 
-sta1.school_id,sta1.book_id,sta1.subject_id,sta1.class_id,sta1.class_name,
-sta1.student_id,sta1.student_name,sta1.integral,sta1.qst_num,0 qst_right,0 qst_right_rate,
-row_number() over(partition by sta1.class_id,sta1.book_id 
-order by sta1.integral desc) class_rank,
-row_number() over(partition by sta1.school_id,sta1.book_id 
-order by sta1.integral desc) school_rank, 
-from_unixtime(unix_timestamp(),'yyyyMMdd') date_time
+UNION ALL
+-- last week records
+SELECT
+sta1.book_id,sta1.subject_id,sta1.class_id,
+sta1.student_id,sta1.integral,sta1.qst_num,INT(sta1.qst_right/100) AS qst_right,
+INT(sta1.qst_right*10/sta1.qst_num)/10 AS qst_right_rate, 2 AS is_weekly
 FROM(
-SELECT a1.student_id,a3.student_name,a1.class_id,a3.class_name,a3.school_id,
+SELECT a1.student_id,a1.class_id,
 a1.book_id,a2.subject_id,a2.book_type,
   SUM(a1.integral) integral,   
 CASE WHEN a2.book_type IN (1,3) THEN COUNT(a1.game_id)*10
     WHEN a2.book_type IN (2,5) THEN COUNT(a1.game_id)
-    END AS qst_num
+    END AS qst_num,
+CASE WHEN a2.book_type IN (1,3) THEN SUM(question_right)*100
+    WHEN a2.book_type IN (2,5) THEN SUM(accuracy)
+    ELSE 0 
+    END AS qst_right
 FROM rptdata.fact_king_game_detail a1 
 JOIN rptdata.fact_king_book_library_detail a2 ON a1.book_id = a2.book_id
+WHERE a1.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+AND a2.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+AND a1.create_time>=(unix_timestamp(substr(from_unixtime(unix_timestamp()),1,4),'yyyy')+
+          ((weekofyear(from_unixtime(unix_timestamp(),'yyyy-MM-dd'))-2)*86400*7))*1000
+AND a1.create_time<(unix_timestamp(substr(from_unixtime(unix_timestamp()),1,4),'yyyy')+
+          ((weekofyear(from_unixtime(unix_timestamp(),'yyyy-MM-dd'))-1)*86400*7))*1000
+GROUP BY a1.student_id,a1.class_id,a1.book_id,a2.subject_id,a2.book_type
+) sta1
+
+UNION ALL 
+-- great last week records
+SELECT 
+sta1.book_id,sta1.subject_id,sta1.class_id,
+sta1.student_id,sta1.integral,sta1.qst_num,INT(sta1.qst_right/100) AS qst_right,
+INT(sta1.qst_right*10/sta1.qst_num)/10 AS qst_right_rate, 4 AS is_weekly
+FROM(
+SELECT a1.student_id,a1.class_id,
+a1.book_id,a2.subject_id,a2.book_type,
+  SUM(a1.integral) integral,   
+CASE WHEN a2.book_type IN (1,3) THEN COUNT(a1.game_id)*10
+    WHEN a2.book_type IN (2,5) THEN COUNT(a1.game_id)
+    END AS qst_num,
+CASE WHEN a2.book_type IN (1,3) THEN SUM(question_right)*100
+    WHEN a2.book_type IN (2,5) THEN SUM(accuracy)
+    ELSE 0 
+    END AS qst_right
+FROM rptdata.fact_king_game_detail a1 
+JOIN rptdata.fact_king_book_library_detail a2 ON a1.book_id = a2.book_id
+WHERE a1.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+AND a2.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+AND a1.create_time>=(unix_timestamp(substr(from_unixtime(unix_timestamp()),1,4),'yyyy')+
+          ((weekofyear(from_unixtime(unix_timestamp(),'yyyy-MM-dd'))-3)*86400*7))*1000
+AND a1.create_time<(unix_timestamp(substr(from_unixtime(unix_timestamp()),1,4),'yyyy')+
+          ((weekofyear(from_unixtime(unix_timestamp(),'yyyy-MM-dd'))-2)*86400*7))*1000
+GROUP BY a1.student_id,a1.class_id,a1.book_id,a2.subject_id,a2.book_type
+) sta1
+
+UNION ALL 
+-- historic records
+SELECT 
+sta1.book_id,sta1.subject_id,sta1.class_id,
+sta1.student_id,sta1.integral,sta1.qst_num,INT(sta1.qst_right/100) AS qst_right,
+INT(sta1.qst_right*10/sta1.qst_num)/10 AS qst_right_rate, 1 AS is_weekly
+FROM(
+SELECT a1.student_id,a1.class_id,
+a1.book_id,a2.subject_id,a2.book_type,
+  SUM(a1.integral) integral,   
+CASE WHEN a2.book_type IN (1,3) THEN COUNT(a1.game_id)*10
+    WHEN a2.book_type IN (2,5) THEN COUNT(a1.game_id)
+    END AS qst_num,
+CASE WHEN a2.book_type IN (1,3) THEN SUM(question_right)*100
+    WHEN a2.book_type IN (2,5) THEN SUM(accuracy)
+    ELSE 0 
+    END AS qst_right
+FROM rptdata.fact_king_game_detail a1 
+JOIN rptdata.fact_king_book_library_detail a2 ON a1.book_id = a2.book_id
+WHERE a1.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+AND a2.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+GROUP BY a1.student_id,a1.class_id,a1.book_id,a2.subject_id,a2.book_type
+) sta1
+)st0
 LEFT JOIN (
 SELECT DISTINCT ta3.student_id,ta3.student_name,
 ta3.class_name,ta3.school_id
 FROM rptdata.fact_king_student_detail ta3
-) a3 
-ON a1.student_id = a3.student_id
-WHERE a1.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
-AND a2.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
-GROUP BY a1.student_id,a3.student_name,a1.class_id,a3.class_name,a3.school_id,
-a1.book_id,a2.subject_id,a2.book_type
-  ) sta1
-)stab1
-)st0;
+WHERE ta3.src_file_day = from_unixtime(unix_timestamp()-60*60*24*1,'yyyyMMdd')
+) st3 
+ON st0.student_id = st3.student_id limit 40;
